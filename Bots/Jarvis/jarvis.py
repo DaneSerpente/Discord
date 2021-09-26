597e5228ae9b4cd94aa40ca6db41c528d1b3863b
DaneSerpete <dane.serpente@gmail.com>
2021-09-26 18:12:23 +1000

    Jarvis update

--- Bots/Jarvis/jarvis.py
+++ Bots/Jarvis/jarvis.py
@@ -11,6 +11,8 @@ ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 NAS01 = [os.getenv('NAS01_IP'), os.getenv('NAS01_UN'), os.getenv('NAS01_PW')]
+NAS02 = [os.getenv('NAS02_IP'), os.getenv('NAS02_UN'), os.getenv('NAS02_PW')]
+Odroid = [os.getenv('Odroid_IP'), os.getenv('Odroid_UN'), os.getenv('Odroid_PW')]
 
 bot = commands.Bot(command_prefix='!')
 
@@ -18,6 +20,14 @@ def disk_space(device):
     if device.lower() == 'nas01':
         ssh.connect(hostname=NAS01[0], username=NAS01[1], password=NAS01[2])
         stdin, stdout, stderr = ssh.exec_command('df -h | grep vg1000')
+        result = stdout.read().decode('utf-8').split()
+        df = [result[1], result[3], result[4]]
+        return df
+        ssh.close()
+
+    if device.lower() == 'nas02':
+        ssh.connect(hostname=NAS02[0], username=NAS02[1], password=NAS02[2])
+        stdin, stdout, stderr = ssh.exec_command('df -h | grep md0')
         result = stdout.read().decode('utf-8').split()
         df = [result[1], result[3], result[4]]
         return df
@@ -35,6 +45,13 @@ async def df(ctx, cmd):
 Total space: {df[0]}
 Used space: {df[2]}""")
     
+@bot.command(name='NAS02', help='Show details for NAS02')
+async def df(ctx, cmd):
+    if cmd.lower() == 'diskspace':
+        df = disk_space('nas02')
+        await ctx.send(f"""Free space: {df[1]}
+Total space: {df[0]}
+Used space: {df[2]}""")
 
 
 bot.run(TOKEN)
